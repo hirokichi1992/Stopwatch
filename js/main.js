3d6f4bdfaee10f1814ad63a8c5c10ca1fab6cf74
hirokichi1992 <plastar14105@gmail.com>
2020-04-20 15:17:03 +0900

    見た目調整

--- js/main.js
+++ js/main.js
@@ -30,24 +30,27 @@
     }
 
     // 開始前の状態
+    // .inactiveクラスのつけ外しで表示切り替え
     function setButtonStateInitial(){
-        start.disabled = false;
-        stop.disabled = true;
-        reset.disabled = true;
+        start.classList.remove('inactive');
+        stop.classList.add('inactive');
+        reset.classList.add('inactive');
     }
 
     // 動作中の状態
+    // .inactiveクラスのつけ外しで表示切り替え
     function setButtonStateRunning(){
-        start.disabled = true;
-        stop.disabled = false;
-        reset.disabled = true;
+        start.classList.add('inactive');
+        stop.classList.remove('inactive');
+        reset.classList.add('inactive');
     }
 
     // ストップを押した後の状態
+    // .inactiveクラスのつけ外しで表示切り替え
     function setButtonStateStopped(){
-        start.disabled = false;
-        stop.disabled = true;
-        reset.disabled = false;
+        start.classList.remove('inactive');
+        stop.classList.add('inactive');
+        reset.classList.remove('inactive');
     }
 
     // ボタンの状態を初期状態に
@@ -55,6 +58,10 @@
 
     // スタートを押した時のイベント定義
     start.addEventListener('click', () => {
+        // .inactiveクラスがある場合、ボタンを押しても反応しないようにする
+        if (start.classList.contains('inactive') === true){
+            return;
+        }
         setButtonStateRunning();
         startTime = Date.now();
         countUp();
@@ -62,6 +69,10 @@
 
     // ストップを押した時のイベント定義
     stop.addEventListener('click', () => {
+        // .inactiveクラスがある場合、ボタンを押しても反応しないようにする
+        if (stop.classList.contains('inactive') === true){
+            return;
+        }
         setButtonStateStopped();
         clearTimeout(timeoutId);
         elapsedTime += Date.now() - startTime;
@@ -69,8 +80,12 @@
 
     // リセットを押した時のイベント定義
     reset.addEventListener('click', () => {
+        // .inactiveクラスがある場合、ボタンを押しても反応しないようにする
+        if (reset.classList.contains('inactive') === true){
+            return;
+        }
         setButtonStateInitial();
-        timer.textContent = '00.00.000';
+        timer.textContent = '00:00.000';
         elapsedTime = 0;
     });
 }
